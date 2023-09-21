38f010fe3d0307df4c8237c56e0efde6754b655a
nxx5xxx <dnwnx123@naver.com>
2023-09-21 23:58:22 +0900

    update junit
    update junit

--- noSQL/mongoDB/mongoDBTest/src/test/java/com/okbs/MongoDbTestApplicationTests.java
+++ noSQL/mongoDB/mongoDBTest/src/test/java/com/okbs/MongoDbTestApplicationTests.java
@@ -1,5 +1,7 @@
 package com.okbs;
 
+import java.util.Scanner;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +26,15 @@ class MongoDbTestApplicationTests {
 	@DisplayName("유저하나만 검색")
 	void printUser() {
 		System.out.println(userRepository.findById("1112").orElseThrow());
+	}
+	
+	@Test
+	@DisplayName("유저업데이트")
+	void updateUser() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("변경할 유저 네임을 입력해주세요");
+		String userName = sc.nextLine();
+		userRepository.findById(userName);
+		
 	}
 }
